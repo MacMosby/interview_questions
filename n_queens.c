1198d9b015c1b7f1b0b0c9ee265273f87d0f10e9
Marc Rodenbusch <marcrodenbusch@gmail.com>
2024-10-20 23:22:02 +0200

    solved n queens problem

--- n_queens.c
+++ n_queens.c
@@ -3,11 +3,86 @@
 /*                                                        :::      ::::::::   */
 /*   n_queens.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mrodenbu <mrodenbu@student.42berlin.de>    +#+  +:+       +#+        */
+/*   By: marcrodenbusch <marcrodenbusch@student.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/20 22:33:34 by mrodenbu          #+#    #+#             */
-/*   Updated: 2024/10/20 22:33:36 by mrodenbu         ###   ########.fr       */
+/*   Updated: 2024/10/20 23:20:03 by marcrodenbu      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
 
+int	safeQ(int *arr, int col, int row)
+{
+	int i = 0;
+
+	while (i < col)
+	{
+		if (arr[i] == row)
+			return (0);
+		if (arr[i] - i == row - col)
+			return (0);
+		if (arr[i] + i == row + col)
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
+void	print_sol(int *arr, int n)
+{
+	int i = 0;
+
+	while (i < n)
+	{
+		if (i == 0)
+			printf("%d", arr[i]);
+		else
+			printf(" %d", arr[i]);
+		i++;
+	}
+	printf("\n");
+}
+
+void	solveNQ(int *arr, int n, int col)
+{
+	int i = 0;
+
+	if (col == n)
+	{
+		print_sol(arr, n);
+		return ;
+	}
+	while (i < n)
+	{
+		if (safeQ(arr, col, i))
+		{
+			arr[col] = i;
+			solveNQ(arr, n, col + 1);
+		}
+		i++;
+	}
+}
+
+int main(int argc, char **argv)
+{
+	if (argc != 2)
+		return (0);
+	else
+	{
+		int n = atoi(argv[1]);
+		if (n < 1)
+			return (0);
+		int *arr = malloc(n * sizeof(int));
+		int i = 0;
+		while (i < n)
+		{
+			arr[i] = -1;
+			i++;
+		}
+		solveNQ(arr, n, 0);
+	}
+	return (0);
+}
