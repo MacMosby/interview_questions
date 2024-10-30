c0ee144461fded5882694b32e1643fda1e07a15b
Marc Rodenbusch <87020460+MacMosby@users.noreply.github.com>
2024-10-30 17:16:45 +0100

    improved sandbox

--- lvl01/sandbox.c
+++ lvl01/sandbox.c
@@ -6,7 +6,7 @@
 /*   By: marcrodenbusch <marcrodenbusch@student.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/30 14:36:24 by marcrodenbu       #+#    #+#             */
-/*   Updated: 2024/10/30 14:47:40 by marcrodenbu      ###   ########.fr       */
+/*   Updated: 2024/10/30 17:16:10 by marcrodenbu      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,5 +25,87 @@ This function must test if the function f is a nice function or a bad function,
 you will return 1 if it is nice, 0 if f is bad or -1 in case of an error in your
 function.
 
-A function is considered
+A function is considered bad if it is terminated or stopped by a signal
+(segfault, abort...), if it exits with any other exit code than 0 or if it times
+out.
+
+If verbose is true, you must write the appropriate message among the following:
+"Nice function!\n"
+"Bad function: exited with code <exit_code>\n"
+"Bad function: <signal_description>\n"
+"Bad function: timed out after <timeout> seconds\n"
+
+You must not leak processes (even in zombie state, this will be checked using
+wait).
+
+We will test your code with very bad functions.
 */
+
+#include <signal.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+#include <stdbool.h>
+#include <string.h>
+#include <errno.h>
+
+int g_to;
+
+void  handle_timeout(int sig)
+{
+  if (sig == SIGALRM)
+    g_to = 1;
+}
+
+int sandbox(void (*f)(void), unsigned int timeout, bool verbose)
+{
+  int pid;
+  int status;
+  struct sigaction sa_to;
+
+  if ((pid = fork()) == -1)
+    return (-1);
+  if (pid == 0)
+  {
+    f();
+    exit(0);
+  }
+  sa_to.sa_handler = handle_timeout;
+  sigaction(SIGALRM, &sa_to, NULL);
+  alarm(timeout);
+  if (waitpid(pid, &status, 0) == -1)
+  {
+     if (errno == EINTR)
+     {
+      if (g_to)
+      {
+        if (verbose)
+          printf("Bad function: timed out after %u seconds\n", timeout);
+        kill(pid, SIGKILL);
+        return (0);
+      }
+     }
+     else
+       return (-1);
+  }
+  if (WIFSIGNALED(status))
+  {
+    int signum = WTERMSIG(status);
+    if (verbose)
+      printf("Bad function: %s\n", strsignal(signum));
+    return (0);
+  }
+  if (WIFEXITED(status) && WEXITSTATUS(status))
+  {
+    if (verbose)
+      printf("Bad function: exited with code %d\n", WEXITSTATUS(status));
+    return (0);
+  }
+  else
+  {
+    if (verbose)
+      printf("Nice function!\n");
+    return (1);
+  }
+}
