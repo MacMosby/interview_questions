c4e0b1778fe786163405d92b779a6e58b6ec065c
Marc Rodenbusch <mrodenbu@student.42berlin.de>
2024-11-15 15:35:09 +0100

    solution using AST

--- lvl02/vbc.c
+++ lvl02/vbc.c
@@ -1,64 +1,140 @@
-#include <ctype.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
 
-int evaluate_expression(char **expr);
+typedef struct node {
+    enum { ADD, MULT, VAL } type;
+    int val;
+    struct node *l;
+    struct node *r;
+} node;
 
-int parse_number(char **expr)
-{
-	int num = 0;
-	if(**expr > 47 && **expr < 58)
-	{
-		num = (**expr) - 48;
-		(*expr)++;
-	}
-	return (num);
+node *parse_expr(char **s);
+
+node* new_node(node n) {
+    node* ret = calloc(1, sizeof(node));
+    if (!ret) return NULL;
+    *ret = n;
+    return ret;
 }
 
-int parse_factor(char **expr)
-{
-	int result;
-	if (**expr == '(')
-	{
-		(*expr)++;
-		result = evaluate_expression(expr);
-		(*expr)++;
-	}
-	else
-	{
-		result = parse_number(expr);
-	}
-	return (result);
+void unexpected(char c) {
+    if (c)
+        printf("Unexpected token '%c'\n", c);
+    else
+        printf("Unexpected end of input\n");
 }
 
-int parse_term(char **expr)
-{
-	int result = parse_factor(expr);
-	while (**expr == '*')
-	{
-		(*expr)++;
-		result *= parse_factor(expr);
-	}
-	return (result);
+int accept(char **s, char c) {
+    if (**s == c) {
+        (*s)++;
+        return 1;
+    }
+    return 0;
 }
 
-int evaluate_expression(char **expr)
-{
-	int result = parse_term(expr);
-	while (**expr == '+')
-	{
-		(*expr)++;
-		result += parse_term(expr);
-	}
-	return (result);
+int expect(char **s, char c) {
+    if (accept(s, c)) {
+        return 1;
+    }
+    unexpected(**s);
+    return 0;
 }
 
-int main(int argc, char **argv)
-{
-	if (argc != 2)
-		return (1);
-	char *expr = argv[1];
-	int result = evaluate_expression(&expr);
+int eval_tree(node* tree) {
+    if (tree->type == ADD)
+        return eval_tree(tree->l) + eval_tree(tree->r);
+    else if (tree->type == MULT)
+        return eval_tree(tree->l) * eval_tree(tree->r);
+    else
+        return tree->val;
+}
 
-	printf("%d\n", result);
-	return (0);
+void destroy_tree(node* tree) {
+    if (tree) {
+        if (tree->type != VAL) {
+            destroy_tree(tree->l);
+            destroy_tree(tree->r);
+        }
+        free(tree);
+    }
+}
+
+// solution below
+
+node *parse_nbr(char **s) {
+    if(isdigit(**s)) {
+        node n = { .type = VAL, .val = **s - '0', .r = NULL, .l = NULL };
+        (*s)++;
+        return new_node(n);
+    }
+    unexpected(**s);
+    return NULL;
+}
+
+node *parse_fact(char **s)
+{
+    if (accept(s, '('))
+    {
+        node *expr = parse_expr(s);
+        if (!expect(s, ')'))
+        {
+            destroy_tree(expr);
+            return NULL;
+        }
+        return expr;
+    }
+    return parse_nbr(s);
+}
+
+
+node *parse_term(char **s) {
+    node *left = parse_fact(s);
+    while (accept(s, '*')) {
+        node *right = parse_fact(s);
+        if (!right) {
+            destroy_tree(left);
+            return NULL;
+        }
+        node n = { .type = MULT , .l = left, .r = right };
+        left = new_node(n);
+    }
+    return left;
+}
+
+node *parse_expr(char **s) {
+    node *left = parse_term(s);
+    while (accept(s, '+')) {
+        node *right = parse_term(s);
+        if (!right) {
+            destroy_tree(left);
+            return NULL;
+        }
+        node n = { .type = ADD, .l = left, .r = right };
+        left = new_node(n);
+    }
+    return left;
+}
+
+int main(int ac, char** av) {
+    if (ac != 2) {
+        return 1;
+    }
+
+    char *input = av[1];
+    node *tree = parse_expr(&input);
+
+    if (!tree) {
+        destroy_tree(tree);
+        return 1;
+    }
+    else if (*input) {
+        unexpected(*input);
+        destroy_tree(tree);
+        return 1;
+    }
+
+    printf("%d\n", eval_tree(tree));
+    destroy_tree(tree);
+    return 0;
 }
